aa2089600b5b39763334c2ae01aefb0b0104a7c8
Geoffroy Couprie <geo.couprie@gmail.com>
2017-09-27 18:15:49 +0200

    remove a useless file

--- main/build.rs
+++
@@ -1,7 +0,0 @@
-use std::env;
-use std::path::Path;
-
-fn main() {
-  let out_dir = env::var("OUT_DIR").expect("OUT_DIR is not defined");
-  let dest_path = Path::new(&out_dir).join("english.rs");
-}
