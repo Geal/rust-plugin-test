a579c9d0dbd3ca8ed5c12ee189a25096ebc8c82b
Geoffroy Couprie <geo.couprie@gmail.com>
2017-09-27 14:59:53 +0200

    add a factory to the plugin info

--- plugin-api/src/lib.rs
+++ plugin-api/src/lib.rs
@@ -1,9 +1,10 @@
 
 pub trait PluginInformation {
   fn name(&self) -> String;
+  fn get_instance(&self) -> Box<SayHello>;
 }
 
 pub trait SayHello {
-  fn say_hello() -> String;
+  fn say_hello(&self) -> String;
 }
 
