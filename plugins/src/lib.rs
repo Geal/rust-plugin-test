ea9ef7d6e926735554b6164344dbfbf14b2ca927
Geoffroy Couprie <geo.couprie@gmail.com>
2017-09-27 12:01:59 +0200

    deactivate the crate loading idea for now

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -1,12 +1,12 @@
 extern crate plugin_test_api as plugin_api;
 extern crate nom;
-extern crate french;
+//extern crate french;
 
 mod english;
 
 // should we do that?
 pub use english::*;
-pub use french::*;
+//pub use french::*;
 
 use plugin_api::{PluginMetadata,PluginInfo};
 use std::collections::hash_map::HashMap;
@@ -18,7 +18,7 @@ use std::collections::hash_map::HashMap;
 pub fn plugins_list() -> HashMap<String,Box<PluginInfo>> {
   let mut h = HashMap::new();
   h.insert("english", Box::new(english::PluginMetadata));
-  h.insert("french", Box::new(french::PluginMetadata));
+  //h.insert("french", Box::new(french::PluginMetadata));
 
   h
 }
